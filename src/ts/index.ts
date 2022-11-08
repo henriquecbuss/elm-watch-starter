cbb630fcc84c5e56c5e6e3e183725ce05b9f71e0
Henrique Buss <henriquebusss@gmail.com>
2022-11-08 19:25:32 -0300

    Remove run function

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,33 +1,29 @@
 import { defineCustomElements } from "../../generated/customElements";
 import * as pattern from "ts-pattern";
 
-const run = () => {
-  defineCustomElements();
-  const counter = localStorage.getItem("counter");
+defineCustomElements();
+const counter = localStorage.getItem("counter");
 
-  const app = window.Elm.Main.init({
-    flags: {
-      counter: counter ? parseInt(counter) : null,
-    },
-  });
+const app = window.Elm.Main.init({
+  flags: {
+    counter: counter ? parseInt(counter) : null,
+  },
+});
 
-  app.ports.interopFromElm.subscribe((fromElm) => {
-    pattern.match(fromElm)
-      .with({ tag: "alert" }, ({ data }) => {
-        console.warn(data.message);
+app.ports.interopFromElm.subscribe((fromElm) => {
+  pattern.match(fromElm)
+    .with({ tag: "alert" }, ({ data }) => {
+      console.warn(data.message);
 
-        app.ports.interopToElm.send({ tag: "alerted" });
-      })
-      .with({ tag: "scrollTo" }, ({ data }) => {
-        document.querySelector(data.querySelector)?.scrollIntoView({
-          behavior: "smooth",
-        });
-      })
-      .with({ tag: "storeCounter" }, ({ data }) => {
-        localStorage.setItem("counter", data.counter.toString());
-      })
-      .exhaustive();
-  });
-};
-
-run();
+      app.ports.interopToElm.send({ tag: "alerted" });
+    })
+    .with({ tag: "scrollTo" }, ({ data }) => {
+      document.querySelector(data.querySelector)?.scrollIntoView({
+        behavior: "smooth",
+      });
+    })
+    .with({ tag: "storeCounter" }, ({ data }) => {
+      localStorage.setItem("counter", data.counter.toString());
+    })
+    .exhaustive();
+});
